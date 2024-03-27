ab3b539f5e2170a2b14263a1c3179bb7bd047c61
kapiaszczyk <ka.piaszczyk@gmail.com>
2024-03-27 14:13:29 +0100

    Optimize and format script

--- jobboard_data_generation_scripts/populate_addresses.py
+++ jobboard_data_generation_scripts/populate_addresses.py
@@ -1,88 +1,82 @@
 from faker import Faker
-from faker.providers import address, company, currency, date_time, file, job, lorem
-from sql_metadata import Parser
+from faker.providers import address
 import sys
+from random import sample
 
-# INSERT INTO address (street, city, postal_code, country, company_id)
-# VALUES (
-#         '1600 Amphitheatre Parkway',
-#         'Mountain View',
-#         '94043',
-#         'USA',
-#         2
-#     );
+fake = Faker()
+
 
 def generate_street_names(amount):
-    fake = Faker()
     fake.add_provider(address)
-    street_names = []
-    for _ in range(amount):
-        street_names.append(fake.street_name())
+    street_names = [fake.street_name() for _ in range(amount)]
     return street_names
 
+
 def generate_cities(amount):
-    fake = Faker()
     fake.add_provider(address)
-    cities = []
-    for _ in range(amount):
-        cities.append(fake.city())
+    cities = [fake.city() for _ in range(amount)]
     return cities
 
+
 def generate_postal_codes(amount):
-    fake = Faker()
     fake.add_provider(address)
-    postal_codes = []
-    for _ in range(amount):
-        postal_codes.append(fake.postcode())
+    postal_codes = [fake.postcode() for _ in range(amount)]
     return postal_codes
 
+
 def generate_countries(amount):
-    fake = Faker()
     fake.add_provider(address)
-    countries = []
-    for _ in range(amount):
-        countries.append(fake.country())
+    countries = [fake.country() for _ in range(amount)]
     return countries
 
-# Since we cannot assign an address to a company that does not exist,
-# make sure to provide valid range for existing company id values (the amount of companies generated previously)
-def generate_company_id(number_of_companies):
-    fake = Faker()
+
+def generate_company_id(amount, number_of_companies):
     company_ids = []
-    for _ in range(number_of_companies):
-        company_ids.append(fake.random_int(min=1, max=number_of_companies))
+
+    # First fill with all company ids
+    for _ in range(1, number_of_companies + 1):
+        company_ids.append(_)
+
+    # For amount of addresses left, add random company ids
+    for _ in range(amount - number_of_companies):
+        company_ids.append(sample(range(1, number_of_companies + 1), 1)[0])
+
     return company_ids
 
-# TODO - Refactor this function
+
 def assemble_queries(street_names, cities, postal_codes, countries, company_ids):
     queries = []
     for i in range(len(company_ids)):
-        query = f"INSERT INTO address (street, city, postal_code, country, company_id) VALUES ('{street_names[i]}', '{cities[i]}', '{postal_codes[i]}', '{countries[i]}', {company_ids[i]});"
+        query = f"INSERT INTO address (street, city, postal_code, country, company_id) VALUES ('{
+            street_names[i]}', '{cities[i]}', '{postal_codes[i]}', '{countries[i]}', {company_ids[i]});"
         queries.append(query)
     return queries
+
 
 def write_queries_to_file(queries):
     with open('populate_addresses.sql', 'w') as file:
         for query in queries:
             file.write(query + '\n')
 
+
 def main():
     if len(sys.argv) < 3:
-        print("Amount of addresses to generate is required as an argument. Example: python populate_addresses.py 10 20")
-        print("Amount of companies generated previously is required as an argument. Example: python populate_addresses.py 10 10")
-        print("Make sure to generate companies first before generating addresses. The company id must exist in the database.")
-        print("Otherwise, the foreign key constraint will fail.")
+        print("Usage: python populate_addresses.py <amount_of_addresses> <number_of_companies>")
+        print("Amount of addresses should be equal or greater than the number of companies. More addresses mean more companies with multiple locations.")
         return
-    
+
     amount = int(sys.argv[1])
     number_of_companies = int(sys.argv[2])
+
     street_names = generate_street_names(amount)
     cities = generate_cities(amount)
     postal_codes = generate_postal_codes(amount)
     countries = generate_countries(amount)
-    company_ids = generate_company_id(number_of_companies)
-    queries = assemble_queries(street_names, cities, postal_codes, countries, company_ids)
+    company_ids = generate_company_id(amount, number_of_companies)
+    queries = assemble_queries(
+        street_names, cities, postal_codes, countries, company_ids)
     write_queries_to_file(queries)
+
 
 if __name__ == "__main__":
     main()

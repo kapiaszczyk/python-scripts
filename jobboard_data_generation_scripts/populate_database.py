d04651e1c0c5d205a1d3e3be26fb90bb28853497
kapiaszczyk <ka.piaszczyk@gmail.com>
2024-03-27 14:27:53 +0100

    Format file

--- jobboard_data_generation_scripts/populate_database.py
+++ jobboard_data_generation_scripts/populate_database.py
@@ -1,15 +1,9 @@
-# Scripts: 
-# populate_technologies.py <amount_of_technologies>
-# populate_companies.py <amount_of_companies>
-# populate_addresses.py <amount_of_addresses> <amount_of_companies> (amount bigger than companies and each company has an address)
-# populate_job_offers.py <amount_of_job_offers>
-# populate_job_offer_technologies.py <amount_of_job_offers> <amount_of_technologies> (amount bigger than job offers and each job offer has at least one technology)
-
 import os
 import sys
 
+
 def main():
-    
+
     if len(sys.argv) < 6:
         print("Usage: python populate_database.py <amount_of_technologies> <amount_of_companies> <amount_of_addresses> <amount_of_job_offers> <amount_of_technologies_job_offers>")
         return
@@ -20,13 +14,10 @@ def main():
     amount_of_job_offers = int(sys.argv[4])
     amount_of_technologies_job_offers = int(sys.argv[5])
 
-    # Check if there is more addresses than companies
-    # Check if there is equal amount technology_job_offers and job_offers
-
     if amount_of_addresses < amount_of_companies:
         print("There must be more addresses than companies.")
         exit(1)
-    
+
     if amount_of_technologies_job_offers < amount_of_job_offers:
         print("There must be more job_offer_technologies than job offers.")
         exit(1)
@@ -38,28 +29,44 @@ def main():
     # Run the scripts
     print("Populating technologies...")
     exit_code = 0
-    exit_code = exit_code or os.system(f"python populate_technologies.py {amount_of_technologies}")
+    exit_code = exit_code or os.system(
+        f"python populate_technologies.py {amount_of_technologies}")
     print("Populating companies...")
-    exit_code = exit_code or os.system(f"python populate_companies.py {amount_of_companies}")
+    exit_code = exit_code or os.system(
+        f"python populate_companies.py {amount_of_companies}")
     print("Populating addresses...")
-    exit_code = exit_code or os.system(f"python populate_addresses.py {amount_of_addresses} {amount_of_companies}")
+    exit_code = exit_code or os.system(f"python populate_addresses.py {
+                                       amount_of_addresses} {amount_of_companies}")
     print("Populating job offers...")
-    exit_code = exit_code or os.system(f"python populate_job_offers.py {amount_of_job_offers} {amount_of_companies}")
+    exit_code = exit_code or os.system(f"python populate_job_offers.py {
+                                       amount_of_job_offers} {amount_of_companies}")
     print("Populating job offer technologies...")
-    exit_code = exit_code or os.system(f"python populate_job_offer_technologies.py {amount_of_job_offers} {amount_of_technologies_job_offers}")
+    exit_code = exit_code or os.system(f"python populate_job_offer_technologies.py {
+                                       amount_of_job_offers} {amount_of_technologies_job_offers}")
 
     if exit_code != 0:
         print("An error occurred while running the scripts.")
+        # Delete the files
+        os.remove("populate_technologies.sql")
+        os.remove("populate_companies.sql")
+        os.remove("populate_addresses.sql")
+        os.remove("populate_job_offers.sql")
+        os.remove("populate_job_offer_technologies.sql")
     else:
         print("Database populated successfully.")
 
     # Rename the files
     # Since inserts are run after table creation, files should be renamed to start with a number
-    os.rename("populate_technologies.sql", "insert-statements/01-populate_technologies.sql")
-    os.rename("populate_companies.sql", "insert-statements/03-populate_companies.sql")
-    os.rename("populate_addresses.sql", "insert-statements/05-populate_addresses.sql")
-    os.rename("populate_job_offers.sql", "insert-statements/07-populate_job_offers.sql")
-    os.rename("populate_job_offer_technologies.sql", "insert-statements/09-populate_job_offer_technologies.sql")
+    os.rename("populate_technologies.sql",
+              "insert-statements/01-populate_technologies.sql")
+    os.rename("populate_companies.sql",
+              "insert-statements/03-populate_companies.sql")
+    os.rename("populate_addresses.sql",
+              "insert-statements/05-populate_addresses.sql")
+    os.rename("populate_job_offers.sql",
+              "insert-statements/07-populate_job_offers.sql")
+    os.rename("populate_job_offer_technologies.sql",
+              "insert-statements/09-populate_job_offer_technologies.sql")
 
 
 if __name__ == "__main__":

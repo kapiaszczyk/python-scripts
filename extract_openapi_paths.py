3f5e26924652fb23250a2356e2bd9e7753d2ac47
Karolina Piaszczyk <41442206+kapiaszczyk@users.noreply.github.com>
2024-04-23 22:59:19 +0200

    Update extract_openapi_paths.py

--- extract_openapi_paths.py
+++ extract_openapi_paths.py
@@ -2,9 +2,9 @@ import argparse
 import json
 import yaml
 
+
 FILE_NAME_MD = "endpoints.md"
 FILE_NAME_TXT = "endpoints.txt"
-USAGE = "Usage: python extract_openapi_paths.py -p <path_to_openapi_documentation> -t <True/False> -y <True/False>"
 
 
 def read_documentation(path):
@@ -55,17 +55,57 @@ def extract_endpoints_from_yaml(documentation_yaml):
     return paths_list
 
 
-def write_to_markdown_table(endpoint_list):
+def extract_endpoint_description(documentation_json):
+    """Extracts endpoint description from OpenAPI documentation in JSON format
+
+    Args:
+        documentation_json (str): OpenAPI documentation in JSON format
+
+    Returns:
+        dict: Dictionary of endpoints and their descriptions
+    """
+    documentation_json = json.loads(documentation_json)
+    paths_json = documentation_json["paths"]
+    paths_dict = {}
+    for path in paths_json:
+        paths_dict[path] = paths_json[path]
+    return paths_dict
+
+
+def extract_endpoint_description_yaml(documentation_yaml):
+    """Extracts endpoint description from OpenAPI documentation in YAML format
+
+    Args:
+        documentation_yaml (str): OpenAPI documentation in YAML format
+
+    Returns:
+        dict: Dictionary of endpoints and their descriptions
+    """
+    documentation_yaml = yaml.safe_load(documentation_yaml)
+    paths_yaml = documentation_yaml["paths"]
+    paths_dict = {}
+    for path in paths_yaml:
+        paths_dict[path] = paths_yaml[path]
+    return paths_dict
+
+
+def write_to_markdown_table(endpoint_dict):
     """Writes the extracted endpoints to a markdown table
 
     Args:
-        endpoint_list (list): List of extracted endpoints
+        endpoint_dict (dict): Dictionary containing endpoint paths and associated data
     """
     with open(FILE_NAME_MD, "w", encoding="UTF-8") as file:
-        file.write("| **Endpoint** | **Description** |\n")
-        file.write("| --- | --- |\n")
-        for endpoint in endpoint_list:
-            file.write(f"| `{endpoint}` |  |\n")
+        file.write("| **Endpoint** | **Method** | **Description** |\n")
+        file.write("| --- | --- | --- |\n")
+        for endpoint, methods in endpoint_dict.items():
+            for method, data in methods.items():
+                try:
+                    summary = data['summary']
+                except KeyError:
+                    print(f"Error: Could not extract summary for endpoint: {endpoint}")
+                    summary = "No summary available"
+                file.write(f"| `{endpoint}` | *{method.upper()}* | {summary} |\n")
 
 
 def write_to_file(endpoint_list):
@@ -108,28 +148,25 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args is None or args.path is None:
-        parser.error(USAGE)
+    if args is None:
+        parser.error("No arguments or path provided")
 
-    if args.yaml and args.path and not args.table:
+    if args.path:
         documentation = read_documentation(args.path)
-        endpoints = extract_endpoints_from_yaml(documentation)
-        write_to_file(endpoints)
-        success_message()
-    elif args.yaml and args.table and args.path:
-        documentation = read_documentation(args.path)
-        endpoints = extract_endpoints_from_yaml(documentation)
-        write_to_markdown_table(endpoints)
-        success_message()
-    elif args.table and args.path:
-        documentation = read_documentation(args.path)
-        endpoints = extract_endpoints_from_json(documentation)
-        write_to_markdown_table(endpoints)
-        success_message()
-    elif args.path and not args.table:
-        documentation = read_documentation(args.path)
-        endpoints = extract_endpoints_from_json(documentation)
-        write_to_file(endpoints)
+        if args.yaml:
+            endpoints = extract_endpoints_from_yaml(documentation)
+            if args.table:
+                descriptions = extract_endpoint_description_yaml(documentation)
+                write_to_markdown_table(descriptions)
+            else:
+                write_to_file(endpoints)
+        else:
+            endpoints = extract_endpoints_from_json(documentation)
+            if args.table:
+                descriptions = extract_endpoint_description(documentation)
+                write_to_markdown_table(descriptions)
+            else:
+                write_to_file(endpoints)
         success_message()
     else:
-        parser.error(USAGE)
+        parser.error("No path provided")
